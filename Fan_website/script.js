7df6dd81e04899eab2e24cab037a0422a4dcfc75
DanielHall-Eardley <srt861hemicharger@gmail.com>
2018-04-13 19:34:25 -0400

    Band Website

---
+++ Fan_website/script.js
@@ -0,0 +1,17 @@
+
+/*my Jquery script*/
+$(document).ready(function () {
+
+    $('#home').click(function () {
+        window.location.href = "index.html"
+    })
+    $('#music').click(function () {
+        window.location.href = "Music.html"
+    })
+    $('#tour').click(function () {
+        window.location.href = "Tour.html"
+    })
+    $('#about').click(function () {
+        window.location.href = "About.html"
+    })
+})
